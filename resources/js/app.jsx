153ae01749f4bbf1635fead8616830ae497a69a8
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2025-11-19 16:56:05 +0800

    add title in the website

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,6 +4,8 @@ import '../css/app.css';
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
 
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+
 createInertiaApp({
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
