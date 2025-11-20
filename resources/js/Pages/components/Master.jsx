a443666544d6a63c7a706eafec49ef8789da329d
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2025-11-20 13:15:14 +0800

    Update Master.jsx
    Adjusted the layout added min-h-screen to the master to implement it across pages

--- resources/js/Pages/components/Master.jsx
+++ resources/js/Pages/components/Master.jsx
@@ -3,12 +3,13 @@ import Navbar from "./Navbar";
 
 export default function Master({ children }) {
     return (
-        <div className="drawer lg:drawer-open">
+        <div className="drawer lg:drawer-open min-h-screen">
             <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content">
+            <div className="drawer-content flex flex-col">
                 <Navbar />
-
-                <main>{children}</main>
+                <main className="flex-1 overflow-auto bg-base-200 p-8">
+                    {children}
+                </main>
             </div>
 
             <Sidebar />
