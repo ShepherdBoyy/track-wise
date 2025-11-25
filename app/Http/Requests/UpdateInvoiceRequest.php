3e455697b2ac83464cb9e05b55ba5cd89c7b7a73
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2025-11-25 16:59:55 +0800

    Create UpdateInvoiceRequest.php

---
+++ app/Http/Requests/UpdateInvoiceRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateInvoiceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "hospital_id" => "sometimes|integer",
+            "invoice_number" => "sometimes|string|max:255",
+            "document_date" => "sometimes|date",
+            "due_date" => "sometimes|date",
+            "amount" => "sometimes|numeric|min:0.01",
+            "description" => "sometimes|string|max:255",
+        ];
+    }
+}
