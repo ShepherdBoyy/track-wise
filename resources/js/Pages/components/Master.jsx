3fa5db4ef51e48810ac962708609b9c7c3eef85c
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2025-11-27 09:13:28 +0800

    Update Master.jsx
    Imported and used pagewrapper on all pages

--- resources/js/Pages/components/Master.jsx
+++ resources/js/Pages/components/Master.jsx
@@ -1,14 +1,14 @@
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
-
+import PageWrapper from "./PageWrapper";
 export default function Master({ children }) {
     return (
         <div className="drawer lg:drawer-open min-h-screen">
             <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col">
                 <Navbar />
-                <main className="flex-1 overflow-auto bg-base-200 p-8">
-                    {children}
+                <main className="flex-1 overflow-auto bg-base-200 px-10 py-10 ">
+                    <PageWrapper>{children}</PageWrapper>
                 </main>
             </div>
 
