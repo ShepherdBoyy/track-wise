87f6b18ea194806fa7bd2524cc5eac7b25b19488
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2026-01-05 16:33:18 +0800

    Update Login.jsx
    Change title

--- resources/js/Pages/Authentication/Login.jsx
+++ resources/js/Pages/Authentication/Login.jsx
@@ -47,7 +47,7 @@ export default function Login() {
                                         color="black"
                                     /> */}
                                 </div>
-                                SaleSync
+                                Invoice Tracker
                             </div>
                         </motion.div>
                     </div>
