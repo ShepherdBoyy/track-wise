15749738f5357a97485f1b5d174c58925db19cb7
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2026-01-19 16:37:29 +0800

    Add HospitalPolicy and NavigationPolicy classes
    Introduces HospitalPolicy for hospital-related authorization and NavigationPolicy for navigation access control. These policies define permission checks for various user actions based on roles and permissions.

---
+++ app/Policies/HospitalPolicy.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Hospital;
+use App\Models\User;
+
+class HospitalPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return $user->hasAnyPermissions(["view_all_hospitals", "view_area_hospitals"]);
+    }
+
+    public function viewAll(User $user): bool
+    {
+        return $user->hasPermission("view_all_hospitals");
+    }
+
+    public function view(User $user, Hospital $hospital): bool
+    {
+        if ($user->hasPermission("view_all_hospitals")) {
+            return true;
+        } else if ($user->hasPermission("view_area_hospitals")) {
+            return $user->areas()->where("areas.id", $hospital->area_id)->exists();
+        } else {
+            return false;
+        }
+    }
+
+    public function create(User $user): bool
+    {
+        return $user->hasPermission("manage_hospitals");
+    }
+
+    public function update(User $user, Hospital $hospital): bool
+    {
+        return $user->hasPermission("manage_hospitals");
+    }
+
+    public function delete(User $user, Hospital $hospital): bool
+    {
+        return $user->hasPermission("manage_hospitals");
+    }
+}
